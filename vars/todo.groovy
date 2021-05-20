8f9c32d5eb4ac9ef7d6626ad6d0cdc105375e0a9
anilkumar-puli <82602260+anilkumar-puli@users.noreply.github.com>
2021-05-20 22:25:57 +0530

    new

---
+++ vars/todo.groovy
@@ -0,0 +1,59 @@
+def call(Map params = [:]) {
+  // Start Default Arguments
+  def args = [
+          NEXUS_IP               : '172.31.14.124',
+  ]
+  args << params
+
+  // End Default + Required Arguments
+  pipeline {
+    agent {
+      label "${args.SLAVE_LABEL}"
+    }
+
+    triggers {
+      pollSCM('* * * * 1-5')
+    }
+
+    environment {
+      COMPONENT     = "${args.COMPONENT}"
+      NEXUS_IP      = "${args.NEXUS_IP}"
+      PROJECT_NAME  = "${args.PROJECT_NAME}"
+      SLAVE_LABEL   = "${args.SLAVE_LABEL}"
+      APP_TYPE      = "${args.APP_TYPE}"
+    }
+
+    stages {
+
+      stage('Build Code & Install Dependencies') {
+        steps {
+          script {
+            build = new nexus()
+            build.code_build("${APP_TYPE}", "${COMPONENT}")
+          }
+        }
+      }
+
+
+      stage('Prepare Artifacts') {
+        steps {
+          script {
+            prepare = new nexus()
+            prepare.make_artifacts("${APP_TYPE}", "${COMPONENT}")
+          }
+        }
+      }
+
+      stage('Upload Artifacts') {
+        steps {
+          script {
+            prepare = new nexus()
+            prepare.nexus(COMPONENT)
+          }
+        }
+      }
+
+    }
+
+  }
+}
