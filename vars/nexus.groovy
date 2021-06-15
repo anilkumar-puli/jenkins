06d8b4f61475046482637b575b9e17a1098f462f
anilkumar-puli <82602260+anilkumar-puli@users.noreply.github.com>
2021-06-15 22:32:28 +0530

    new

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -17,7 +17,7 @@ def make_artifacts(APP_TYPE, COMPONENT) {
     def execute_com=sh(returnStdout: true, script: command)
     print execute_com
   } else if(APP_TYPE == "NODEJS") {
-    command = "zip -r ${FILENAME} node_modules server.js"
+    command = "zip -r ${FILENAME} . "
     def execute_com=sh(returnStdout: true, script: command)
     print execute_com
   } else if(APP_TYPE == "JAVA") {
