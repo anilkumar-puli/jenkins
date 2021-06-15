8e2b312ea93b2b8c1e3af0daa9cae1a8900fc2ab
anilkumar-puli <82602260+anilkumar-puli@users.noreply.github.com>
2021-06-15 22:23:05 +0530

    new

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -3,7 +3,7 @@ def nexus(COMPONENT) {
   def get_branch_exec=sh(returnStdout: true, script: get_branch)
   def FILENAME=COMPONENT+'-'+get_branch_exec+'.zip'
 
-  command = "curl -f -v -u admin:admin --upload-file ${FILENAME} http://172.31.28.186:8081/repository/${COMPONENT}/${FILENAME}"
+  command = "curl -f -v -u admin:admin --upload-file ${FILENAME} http://34.229.77.31:8081/repository/${COMPONENT}/${FILENAME}"
   def execute_state=sh(returnStdout: true, script: command)
 }
 
